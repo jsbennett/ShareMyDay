cd89f5aff325436bcc0cce0a72e1e83625c59ff2
jsbennett <bnnttjy@googlemail.com>
2019-02-04 15:47:33 +0000

    Began NFC work - Added NFC Capaibilites in manifest - Added OnResume in MainActivity so the app can use the NFC whilst it is on

--- ShareMyDay/MainActivity.cs
+++ ShareMyDay/MainActivity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Android.App;
+using Android.Nfc;
 using Android.OS;
 using Android.Runtime;
 using Android.Support.Design.Widget;
@@ -9,10 +10,19 @@ using Android.Widget;
 
 namespace ShareMyDay
 {
+    /*
+     * Class Name: Main Activity
+     * Purpose: To be the main entry point for the app which controls the flow
+     */
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private NfcAdapter nfcAdapter;
 
+        /*
+         * Method Name: OnCreate
+         * Purpose: It is used for when the app is loading 
+         */
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -27,6 +37,15 @@ namespace ShareMyDay
             Database.Database db = new Database.Database(System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments),"ShareMyDay.db3");
             db.CreateDatabase();
             db.DatabaseDefaultSetup();
+        }
+
+        /*
+         * Method Name: OnResume
+         * Purpose: This is for when the app has loaded
+         */
+        protected override void OnResume()
+        {
+            base.OnResume();
         }
 
         public override bool OnCreateOptionsMenu(IMenu menu)
