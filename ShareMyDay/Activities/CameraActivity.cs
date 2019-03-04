88a0a53ef86de0dff476546ab9f32deb5f6d4bf8
jsbennett <bnnttjy@googlemail.com>
2019-03-04 12:00:07 +0000

    Added button background for the child home page

--- ShareMyDay/Activities/CameraActivity.cs
+++ ShareMyDay/Activities/CameraActivity.cs
@@ -1,15 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿
 using Android.App;
 using Android.Content;
-using Android.Content.PM;
 using Android.OS;
-using Android.Provider;
-using Android.Runtime;
-using Android.Views;
 using Android.Widget;
 using ShareMyDay.UIComponents;
 
